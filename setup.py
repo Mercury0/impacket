5128f5c2f4c68121065fa6cede3008ef87a9b62c
Mercury0 <mfox05@gmail.com>
2024-09-11 10:48:04 -0700

    Update required setup packages

--- setup.py
+++ setup.py
@@ -72,8 +72,8 @@ setup(
     data_files=data_files,
 
     install_requires=['pyasn1>=0.2.3', 'pyasn1_modules', 'pycryptodomex', 'pyOpenSSL==24.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
-                      'ldapdomaindump>=0.9.0', 'flask>=1.0', 'setuptools', 'charset_normalizer'],
-    extras_require={':sys_platform=="win32"': ['pyreadline3'],
+                      'ldapdomaindump>=0.9.0', 'flask>=1.0', 'future', 'setuptools', 'charset_normalizer'],
+    extras_require={'pyreadline:sys_platform=="win32"': [],
                     },
     classifiers=[
         "Programming Language :: Python :: 3.12",
